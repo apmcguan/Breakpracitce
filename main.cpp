12ac2775d58d348619db1606f5bda9d31a1580ee
apmcguan <111770168+apmcguan@users.noreply.github.com>
2022-09-24 00:18:52 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int main() {
+  //declare variables
+  int NumberData1;
+  //load input file 
+  ifstream inFile; //declare stream variable
+  inFile.open(" NumberData1.txt");
+
+  //load all data in while loop
+  while (!inFile.eof())
+    {
+      //inFile >> NumberData1;
+      //cout << NumberData1 <<endl;
+    }
+  
+  
+}
